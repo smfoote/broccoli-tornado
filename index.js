fe9673b91cfce5fc5105da2563e34cc6323804c1
Steven Foote <smfoote@gmail.com>
2015-07-24 16:47:48 -0600

    Prepare plugin for publishing

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
 'use strict';
-Object.defineProperty(exports, '__esModule', {
-	value: true
-});
+var Filter =  require('broccoli-filter');
+var tornado = require('tornado.js');
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-var _broccoliFilter = require('broccoli-filter');
-
-var _broccoliFilter2 = _interopRequireDefault(_broccoliFilter);
-
-var _tornadoJs = require('tornado.js');
-
-var _tornadoJs2 = _interopRequireDefault(_tornadoJs);
-
-function Tornado(inputTree, options) {
-	// No options yet, but will use options for compiler options
-	options = options || {};
+function Tornado(inputNode, options) {
+  // No options yet, but will use options for compiler options
+  options = options || {};
 	if (!(this instanceof Tornado)) {
-		return new Tornado(inputTree, options);
+		return new Tornado(inputNode, options);
 	}
-
-	this.inputTree = inputTree;
+	Filter.call(this, inputNode, options);
 }
 
-Tornado.prototype = Object.create(_broccoliFilter2['default'].prototype);
+Tornado.prototype = Object.create(Filter.prototype);
 Tornado.prototype.constructor = Tornado;
 
-Tornado.prototype.extensions = ['dust'];
+Tornado.prototype.extensions = ['td', 'tl', 'tmpl'];
 Tornado.prototype.targetExtension = 'js';
 
 Tornado.prototype.processString = function (str, relativePath) {
-	return _tornadoJs2['default'].compile(str, relativePath, this.options);
+  console.log('str', str);
+	return tornado.compile(str, relativePath, this.options);
 };
 
-exports['default'] = Tornado;
-module.exports = exports['default'];
+module.exports = Tornado;
